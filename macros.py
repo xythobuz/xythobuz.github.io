5ff97d94dee017e326868504207155b5937cd520
Thomas Buck <xythobuz@xythobuz.de>
2022-05-25 21:18:56 +0200

    set feed encoding to utf-8

--- macros.py
+++ macros.py
@@ -443,7 +443,7 @@ def hook_preconvert_sitemap():
 # rss feed generation
 # -----------------------------------------------------------------------------
 
-_RSS = """<?xml version="1.0"?>
+_RSS = """<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 <title>%s</title>
